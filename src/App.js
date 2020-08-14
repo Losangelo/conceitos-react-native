9614798af84b03bc4febcdb2a58d5880f9f003be
Losangelo Soares de Sousa Pacífico <losangelo@gmail.com>
2020-08-13 21:24:24 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   SafeAreaView,
@@ -10,72 +10,72 @@ import {
   TouchableOpacity,
 } from "react-native";
 
-import api from './services/api';
+import api from './services/api'
 
 export default function App() {
-  const [repositories, setRepositories] = useState([]);
 
-  useEffect(()=>{
-    api.get('/repositories').then(response => {
-      setRepositories(response.data)
-    });
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
   }, []);
 
   async function handleLikeRepository(id) {
-    await api.post(`/repositories/${id}/like`);
-    setRepositories(
-      repositories.map(
-        repository => (repository.id !== id ? repository : repository.like++ )
-        )
-      )
-  };
- 
+    await api.post(`repositories/${id}/like`);
+
+    const newRepositories = repositories.map( item => {
+      if (item.id == id) {   
+        item.likes += 1;
+      }
+      return item;
+    });
+
+    setRepositories(newRepositories);
+
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <FlatList 
-          data={repositories} 
-          keyExtractor={repository => repository.id} 
-          renderItem={
-            ({ item: repository }) => (
-              <View style={styles.repositoryContainer}>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={( { item } ) =>  (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{item.title}</Text>
 
-                <Text style={styles.repository}>
-                  {repository.title}
-                </Text>
-
-                <View style={styles.techsContainer}>
-                  {repository.tech.map(tech => (
-                    <Text key={tech} style={styles.tech}>
-                      {tech}
-                    </Text>
-                  ))}
-                </View>
-
-                <View style={styles.likesContainer}>
-                  <Text style={styles.likeText}
-                    testID={`repository-likes-${repository.id}`}
-                  >
-                    {repository.likes} curtida{repository.likes > 1 && 's'}
-                  </Text>
-                </View>
-
-                <TouchableOpacity style={styles.button}
-                  onPress={() => handleLikeRepository(repository.id)}
-                  testID={`like-button-${repository.id}`}
-                >
-                  <Text style={styles.buttonText}>Curtir</Text>
-                </TouchableOpacity>
-
+              <View style={styles.techsContainer}>
+                {item.techs.map(tech => <Text style={styles.tech} key={tech}>{tech}</Text>)}
               </View>
-            )
-          }
+
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
+                  testID={`repository-likes-${item.id}`}
+                >
+                  {item.likes} curtidas
+                </Text>
+              </View>
+
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => handleLikeRepository(item.id)}
+                // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
+                testID={`like-button-${item.id}`}
+              >
+                <Text style={styles.buttonText}>Curtir</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         />
       </SafeAreaView>
     </>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
